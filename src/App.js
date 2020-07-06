f829a64481e2c57b0caab0177e7efb8502ed4ed2
elkhan199 <ismayilzadeelxan@gmail.com>
2020-07-06 21:20:29 +0900

    added router

--- src/App.js
+++ src/App.js
@@ -1,11 +1,19 @@
 import React from 'react';
 import './App.css';
 import HomePage from './pages/homepage/homepage.component'
+import {Route} from 'react-router-dom'
+
 
 function App() {
+  const HatsPage  = () => {
+    return(
+      <h1>HatsPage</h1>
+    )
+  }
   return (
     <div >
-      <HomePage/>
+        <Route exact path = '/' component={HomePage} />
+        <Route exact path = '/hats' component={HatsPage} />
     </div>
   );
 }
