3a550287c827497f54375d450b0fdb6a6a0cd85c
elkhan199 <ismayilzadeelxan@gmail.com>
2020-07-10 18:33:43 +0900

    adding selectors for memorization

--- src/components/cart-dropdown/cart-dropdown.component.jsx
+++ src/components/cart-dropdown/cart-dropdown.component.jsx
@@ -4,6 +4,8 @@ import CustomButton from '../custom-button/custom-button.component'
 
 import  './cart-dropdown.styles.scss'
 import CartItem from '../cart-item/cart-item.component'
+import {selectCartItems} from '../../redux/cart/cart.selectors'
+
 const CartDropDown = ({cartItems}) =>(
     <div className='cart-dropdown'>
         <div className='cart-items'>
@@ -15,8 +17,8 @@ const CartDropDown = ({cartItems}) =>(
             <CustomButton>GO TO CHECKOUT</CustomButton>
     </div>
 )
-const mapStateToProps = ({cart: {cartItems}}) => ({
-    cartItems
+const mapStateToProps = (state) => ({
+    cartItems : selectCartItems(state)
 })
 
 export default connect(mapStateToProps)(CartDropDown)
