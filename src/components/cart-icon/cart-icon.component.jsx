afabbf6db2babb21999fc9c7a4526110e6179ce4
elkhan199 <ismayilzadeelxan@gmail.com>
2020-07-10 19:31:36 +0900

    checkout page is made

--- src/components/cart-icon/cart-icon.component.jsx
+++ src/components/cart-icon/cart-icon.component.jsx
@@ -5,7 +5,7 @@ import './cart-icon.styles.scss'
 import {connect} from 'react-redux'
 import {toggleCartHidden} from '../../redux/cart/cart.actions'
 import {selectCartItemsCount} from '../../redux/cart/cart.selectors'
-
+import {createStructuredSelector} from 'reselect'
 
 const CartIcon = ({toggleCartHidden, itemCount}) => (
 <div className = 'cart-icon' onClick = {toggleCartHidden}>
@@ -17,9 +17,9 @@ const mapDispatchToProps = dispatch => ({
     toggleCartHidden: () => dispatch(toggleCartHidden())
 })
 
-const mapStateToProps = (state) => (
+const mapStateToProps = createStructuredSelector (
     {
-        itemCount: selectCartItemsCount(state)
+        itemCount: selectCartItemsCount
     }
 )
 
