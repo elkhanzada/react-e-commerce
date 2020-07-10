8a65adb02eded1fee77316a104cb1cd53b2f5207
elkhan199 <ismayilzadeelxan@gmail.com>
2020-07-10 18:15:18 +0900

    modifying checkout dropdown

--- src/components/cart-dropdown/cart-dropdown.component.jsx
+++ src/components/cart-dropdown/cart-dropdown.component.jsx
@@ -1,15 +1,22 @@
 import React from 'react'
-
+import {connect} from 'react-redux'
 import CustomButton from '../custom-button/custom-button.component'
 
 import  './cart-dropdown.styles.scss'
-
-const CartDropDown = () =>(
+import CartItem from '../cart-item/cart-item.component'
+const CartDropDown = ({cartItems}) =>(
     <div className='cart-dropdown'>
         <div className='cart-items'>
-            <CustomButton>GO TO CHECKOUT</CustomButton>
+            {
+                cartItems.map(cartItem => <CartItem key = {cartItem.id} item={cartItem}/>)
+            }
+
         </div>
+            <CustomButton>GO TO CHECKOUT</CustomButton>
     </div>
 )
+const mapStateToProps = ({cart: {cartItems}}) => ({
+    cartItems
+})
 
-export default CartDropDown
+export default connect(mapStateToProps)(CartDropDown)
