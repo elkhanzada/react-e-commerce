cae6e107003b1ab056b78066fc419a41e5ed14cb
elkhan199 <ismayilzadeelxan@gmail.com>
2020-07-14 20:09:07 +0900

    small changes

--- README.md
+++ README.md
@@ -1,7 +1,6 @@
 # E-commerce Website in React
 The website has basic functionalities such as adding items to the cart and checkout page. 
 Sign in with Email and Sign in with Google options are available in the website. 
-For backend server, firebase is integrated to store user's information.
-Integration of Stripe as a payment method is planned.
+For backend server, firebase is integrated to store user's information. As a payment method, Stripe is integrated.
 
 
--- src/App.js
+++ src/App.js
@@ -39,7 +39,7 @@ class App extends React.Component {
     <div >
       <Header />
         <Switch>
-          <Route exact path = '/' component={HomePage} />
+          <Route exact path = '/react-e-commerce' component={HomePage} />
           <Route  path = '/shop' component={ShopPage} />
           <Route exact path = '/checkout' component={CheckoutPage}/>
           <Route exact path = '/signin' render={() => this.props.currentUser? (<Redirect to='/'/>):(<SignInAndSignUpPage/>)}/>
