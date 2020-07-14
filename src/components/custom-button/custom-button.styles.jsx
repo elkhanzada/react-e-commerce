042b79735f95f619f38776453ffc99a62008fb99
elkhan199 <ismayilzadeelxan@gmail.com>
2020-07-14 21:46:57 +0900

    some fixes

--- src/components/custom-button/custom-button.styles.jsx
+++ src/components/custom-button/custom-button.styles.jsx
@@ -9,16 +9,16 @@ const buttonStyles = css`
         background-color: white;
         color: black;
         border: 1px solid black;
-        }
+    }
 `
 const invertedButtonStyles = css`
     background-color: white;
     color: black;
     border: 1px solid black;
     &:hover {
-    background-color: black;
-    color: white;
-    border: none;
+        background-color: black;
+        color: white;
+        border: none;
     }
 `
 
@@ -26,8 +26,8 @@ const googleSignInStyles = css`
     background-color: #4285f4;
     color: white;
     &:hover {
-    background-color: #357ae8;
-    border: none;
+        background-color: #357ae8;
+        border: none;
     }
 `
 const getButtonStyles = props => {
@@ -35,7 +35,7 @@ const getButtonStyles = props => {
         return googleSignInStyles
     }
 
-    return props.inverted? invertedButtonStyles: buttonStyles
+    return props.inverted ? invertedButtonStyles: buttonStyles
 }
 export const CustomButtonContainer = styled.button`
     min-width: 165px;
